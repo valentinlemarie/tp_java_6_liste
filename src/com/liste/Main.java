c02f29fca2fcb645a5c2a26df6d4c67862d7dcd9
valentinlemarie <valentin.lemarie@wanadoo.fr>
2018-04-13 15:11:45 +0200

    retier tout les hommes de la liste Iterateur Iterateur affichage

--- src/com/liste/Main.java
+++ src/com/liste/Main.java
@@ -1,22 +1,27 @@
 package com.liste;
 
 import java.sql.DatabaseMetaData;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
+        /**
+         * creation d'une liste de Personnes / User  et remplissage
+         */
         ArrayList<User> users = new ArrayList<>();
+
+
         for (int i = 0; i < 10; i++) {
             users.add(new User());
         }
 
 
+        /**
+         * tri alphabetic compare
+         */
         Collections.sort(users, new Comparator<User>() {
             @Override
             public int compare(User o1, User o2) {
@@ -24,15 +29,27 @@ public class Main {
             }
         });
 
+        /**
+         * affichage des users
+         */
         for (int i = 0; i <users.size() ; i++) {
             System.out.println(users.get(i).toString());
         }
         System.out.println("");
         System.out.println("");
+
+        /**
+         * retire tout les hommes de la liste
+         */
         users.removeIf(User::isIs_Male);
 
-        for (int i = 0; i <users.size() ; i++) {
-            System.out.println(users.get(i).toString());
+        /**
+         * affichage des users avec un iterateur
+         */
+        Iterator itr = users.iterator();
+        while (itr.hasNext()){
+            User element = (User) itr.next();
+            System.out.println(element.toString());
         }
     }
 }
